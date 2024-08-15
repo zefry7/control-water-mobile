e2e6ddf260e94c9bf87d36d7b36acf64ec7f7ed6
nikita petrov <petrov.nikita702@mail.ru>
2024-08-15 23:12:45 +0300

    Добавление регулировки количества стаканов и даты

--- app/index.tsx
+++ app/index.tsx
@@ -2,20 +2,27 @@ import { useEffect, useState } from "react";
 import { Text, View, StyleSheet, SafeAreaView, Image, TouchableWithoutFeedback, Animated } from "react-native";
 import AnimatedProgressWheel from 'react-native-progress-wheel';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import moment from 'moment';
+
+var now = moment().format("DD/MM");
 
 export default function Index() {
   const [maxDay, setMaxDay] = useState(10)
   const [waterControl, setWaterControl] = useState(0)
   const [count, setCount] = useState(0)
-  const mass = useState(new Array(10).fill(0))[0]
+  const [mass, setMass] = useState(new Array(10).fill(0))
   const animGlass = useState(new Animated.Value(130))[0]
 
 
   useEffect(() => {
     const ttt = async () => {
       let a = await AsyncStorage.getItem("count")
-      if (a) {
-        setCount(Number(a))
+      if (!a) {
+        // setCount(Number(a))
+      } else {
+        let num = Number(prompt("Сколько воды?"))
+        setMaxDay(num)
+        setMass(new Array(num).fill(0))
       }
     }
     ttt()
@@ -23,7 +30,7 @@ export default function Index() {
 
   const handleClickButton = () => {
     if (maxDay > count) {
-      setWaterControl(water => water + 10);
+      setWaterControl(water => water + 100/maxDay);
       setCount(count => count + 1)
 
       Animated.timing(animGlass, {
@@ -53,26 +60,26 @@ export default function Index() {
       </View>
       <View style={styles.main}>
         <View style={{ position: "relative", flex: 1, justifyContent: "center", alignItems: "center", top: -100 }}>
+          <Text style={{ fontSize: 24, lineHeight: 24, color: '#1976D2', fontWeight: 500, marginBottom: 10 }}>{now}</Text>
           <View style={styles.wrapperCircle}>
             <AnimatedProgressWheel size={350} width={10} color={'#1976D2'} progress={waterControl} backgroundColor={'#BBDEFB'} rotation="-90deg" duration={600} />
           </View>
-          {count != maxDay
-            ? <>
-              <View style={{ maxHeight: 150, width: 150, flex: 1, justifyContent: "center", alignItems: "center", marginBottom: 10 }}>
-                <TouchableWithoutFeedback style={{ height: 350, width: 350 }} onPress={handleClickButton}>
-                  <Animated.View style={{ height: animGlass }}>
-                    <Image source={{ uri: "assets/images/glass.svg" }} style={{ height: "100%", width: 130, cursor: "pointer" }} />
-                  </Animated.View>
-                </TouchableWithoutFeedback>
-              </View>
-              <Text style={{ fontSize: 24, lineHeight: 24, color: '#1976D2', fontWeight: 500 }}>{count}/10</Text>
-            </>
+          {count < maxDay
+            ?
+            <View style={{ maxHeight: 150, width: 150, flex: 1, justifyContent: "center", alignItems: "center", marginBottom: 10 }}>
+              <TouchableWithoutFeedback style={{ height: 350, width: 350 }} onPress={handleClickButton}>
+                <Animated.View style={{ height: animGlass }}>
+                  <Image source={{ uri: "assets/images/glass.svg" }} style={{ height: "100%", width: 130, cursor: "pointer" }} />
+                </Animated.View>
+              </TouchableWithoutFeedback>
+            </View>
             : <View style={{ maxHeight: 150, width: 150, flex: 1, justifyContent: "center", alignItems: "center", marginBottom: 10 }}>
               <Image source={{ uri: "assets/images/accept.png" }} style={{ height: "100%", width: 130, cursor: "pointer" }} />
             </View>
           }
+          <Text style={{ fontSize: 24, lineHeight: 24, color: '#1976D2', fontWeight: 500 }}>{count}/{maxDay}</Text>
         </View>
-        <View style={{ flexWrap: "wrap", flexDirection: "row", gap: 10, width: 340, position: "absolute", bottom: 180 }}>
+        <View style={{ flexWrap: "wrap", flexDirection: "row", gap: 10, maxWidth: 340, position: "absolute", bottom: 120 }}>
           {mass.map((_, i) => {
             if (count >= i + 1) {
               return <Image source={{ uri: "assets/images/glass.svg" }} key={i} style={{ height: 60, width: 60, opacity: 1 }} />
@@ -100,9 +107,6 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     justifyContent: "center",
     alignItems: "center",
-    position: "absolute",
-    top: 0,
-    left: 0,
   },
   headerText: {
     fontSize: 24,
